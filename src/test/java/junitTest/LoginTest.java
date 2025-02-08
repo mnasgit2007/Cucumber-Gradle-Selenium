f5126c777285fc43aa1cf939a2c88e78d1972ae4
mnasgit2007 <130423463+mnasgit2007@users.noreply.github.com>
2025-02-08 21:36:20 +0800

    Add files via upload

---
+++ src/test/java/junitTest/LoginTest.java
@@ -0,0 +1,35 @@
+package junitTest;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.devtools.v85.log.Log;
+import pages.HomePage;
+import pages.LoginPage;
+
+import java.time.Duration;
+
+public class LoginTest {
+
+    @Test
+    public void loginTest(){
+        // inisialisasi webdriver
+        WebDriver driver = WebDriverManager.chromiumdriver().create();
+
+        // inisialisasi page object
+        LoginPage loginPage = new LoginPage(driver);
+        HomePage homePage = new HomePage(driver);
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.saucedemo.com/");
+
+        // action test
+        loginPage.inputUsername("standard_user");
+        loginPage.inputPassword("secret_sauce");
+        loginPage.clickLoginButton();
+
+        // test validation
+        homePage.validateOnHomePage();
+
+    }
+}
