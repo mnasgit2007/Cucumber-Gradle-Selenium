59b945540afd9633309940fd776e27f261fbed35
M Natsir <natsir07@gmail.com>
2025-02-08 17:41:25 +0800

    perubahan

--- src/test/java/runner/CucumberTest.java
+++ src/test/java/runner/CucumberTest.java
@@ -6,9 +6,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = {"pretty"},
         glue = {"stepDef"},
-        features = {"src/test/java/features"},
-        plugin = {"pretty"}
+        features = {"src/test/java/features"}
 )
 
 public class CucumberTest {
