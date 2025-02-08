b332cf0a802afed32f9aeae385ac9c7e45a33152
M Natsir <natsir07@gmail.com>
2025-02-08 17:11:35 +0800

    Cucumber, Java, Gradle, and Selenium

---
+++ src/test/java/helper/BaseTest.java
@@ -0,0 +1,25 @@
+package helper;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+public class BaseTest {
+    protected static WebDriver driver;
+
+    protected void getDriver(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver = WebDriverManager.chromiumdriver().create();
+    }
+}
