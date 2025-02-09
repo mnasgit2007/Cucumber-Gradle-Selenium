895ad3eccbaa8d0be2bd03747d817a72f1c99c84
mnasgit2007 <130423463+mnasgit2007@users.noreply.github.com>
2025-02-10 00:32:49 +0800

    Update CucumberTest.java

--- src/test/java/runner/CucumberTest.java
+++ src/test/java/runner/CucumberTest.java
@@ -8,7 +8,8 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {"pretty", "html:target/cucumber-reports.html"},
         glue = {"stepDef"},
-        features = {"src/test/java/features"}
+        features = {"src/test/java/features"},
+        tags = "login"
 )
 
 public class CucumberTest {
