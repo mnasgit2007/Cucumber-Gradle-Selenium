b717f9c64b1a154fafc81c47ef1d17f47317565c
mnasgit2007 <130423463+mnasgit2007@users.noreply.github.com>
2025-02-10 00:27:20 +0800

    Update BaseTest.java

--- src/test/java/helper/BaseTest.java
+++ src/test/java/helper/BaseTest.java
@@ -1,14 +1,28 @@
 package helper;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.io.File;
 import java.time.Duration;
 
 public class BaseTest {
     protected static WebDriver driver;
+
+    public static File getJSONSchemaFile(String JSONFile){
+        return new File("src/test/java/helper/JSONSchemaData/" + JSONFile);
+    }
+
+    static String generateRandomEmail() {
+        String allowedChart = "abcdefghijklmnopqrstuvwxyz" + "1234567890" + "_-.";
+        String email = "";
+        String temp = RandomStringUtils.randomAlphanumeric(10); // panjang email 10 karakter
+        email = temp + "@testdata.com";
+        return email;
+    }
 
     protected void getDriver(){
         // Mengatur ChromeDriver menggunakan WebDriverManager
@@ -25,4 +39,7 @@ public class BaseTest {
         // Mengatur timeout implisit
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
+
+        public static void quitDriver(){
+        driver.quit();
 }
