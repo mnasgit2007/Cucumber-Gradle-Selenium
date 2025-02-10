5f8fd022aa61cadcc419c0dfd8edda6794edc2ff
mnasgit2007 <130423463+mnasgit2007@users.noreply.github.com>
2025-02-10 13:26:39 +0800

    Update LoginTest.java

--- Cucumber-Gradle-Selenium/src/test/java/junitTest/LoginTest.java
+++ Cucumber-Gradle-Selenium/src/test/java/junitTest/LoginTest.java
@@ -1,35 +1,49 @@
 package junitTest;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.devtools.v85.log.Log;
-import pages.HomePage;
-import pages.LoginPage;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
 public class LoginTest {
 
-    @Test
-    public void loginTest(){
-        // inisialisasi webdriver
-        WebDriver driver = WebDriverManager.chromedriver().create();
+    private WebDriver driver;
 
-        // inisialisasi page object
+    @BeforeEach
+    public void setUp() {
+        // Initialize WebDriver
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void loginTest() {
+        // Initialize page objects
         LoginPage loginPage = new LoginPage(driver);
         HomePage homePage = new HomePage(driver);
 
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://www.saucedemo.com/");
 
-        // action test
+        // Action test
         loginPage.inputUsername("standard_user");
         loginPage.inputPassword("secret_sauce");
         loginPage.clickLoginButton();
 
-        // test validation
+        // Test validation
         homePage.validateOnHomePage();
+    }
 
+    @AfterEach
+    public void tearDown() {
+        // Close the driver after the test
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
